30a6384342bca7ff5a319d5c04843596abeefc61
Mike Sheldrake <mike.sheldrake@gmail.com>
2018-08-30 19:27:20 -0700

    version bump, Changes

--- lib/Math/Geometry/Delaunay.pm
+++ lib/Math/Geometry/Delaunay.pm
@@ -11,7 +11,7 @@ our $VERSION;
 
 BEGIN {
     use XSLoader;
-    $VERSION = '0.19';
+    $VERSION = '0.20';
     XSLoader::load('Math::Geometry::Delaunay');
     exactinit();
     }
@@ -1548,7 +1548,7 @@ Math::Geometry::Delaunay - Quality Mesh Generator and Delaunay Triangulator
 
 =head1 VERSION
 
-Version 0.19
+Version 0.20
 
 =cut
 
